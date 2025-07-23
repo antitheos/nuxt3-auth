ced99d4dd40d2e64950aae9a7e8fe6677d5c8ccf
antitheos <antitheos@gmail.com>
2025-07-22 18:00:43 -0700

    Update module.ts

--- src/module.ts
+++ src/module.ts
@@ -14,7 +14,6 @@ export default defineNuxtModule({
         version,
         configKey: CONFIG_KEY,
         compatibility: {
-            nuxt: '^3.0.0',
         },
     },
     defaults: moduleDefaults,
